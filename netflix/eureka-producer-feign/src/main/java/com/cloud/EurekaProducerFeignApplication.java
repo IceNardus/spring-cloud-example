ff8b778d73b342251bde39bb7e27aabf0b82d231
IceNardus <xch3411@163.com>
2018-05-18 17:17:34 +0800

    添加Hystrix 监控面板 和 endpoint
    Signed-off-by: IceNardus <xch3411@163.com>

--- netflix/eureka-producer-feign/src/main/java/com/cloud/EurekaProducerFeignApplication.java
+++ netflix/eureka-producer-feign/src/main/java/com/cloud/EurekaProducerFeignApplication.java
@@ -2,10 +2,14 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaProducerFeignApplication {
 
 	public static void main(String[] args) {
